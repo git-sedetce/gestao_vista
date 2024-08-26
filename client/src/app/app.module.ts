0e701b78fc660214d046781f4785ace0f750d20f
danielAraujo1406 <daniel.araujo@sedet.ce.gov.br>
2024-08-26 09:47:05 -0300

    montando as páginas

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/estrutura/header/header.component';
+import { FooterComponent } from './components/estrutura/footer/footer.component';
+import { HomeComponent } from './components/paginas/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
