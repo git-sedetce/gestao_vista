ed894e59cf5952508558dbcf8dbfab58fec32e1e
danielAraujo1406 <daniel.araujo@sedet.ce.gov.br>
2024-08-26 11:25:59 -0300

    configurando models

--- client/src/app/shared/models/acompanhamento.model.ts
+++ client/src/app/shared/models/acompanhamento.model.ts
@@ -1,2 +1,9 @@
 export class Acompanhamento {
+  constructor(
+    public situacao_atual: string,
+    public resultado: string,
+    public custo_realizado: string,
+    public leads_realizados: string,
+    public evento_id: number
+  ){}
 }
--- client/src/app/shared/models/evento.model.ts
+++ client/src/app/shared/models/evento.model.ts
@@ -1,2 +1,18 @@
 export class Evento {
+  constructor(
+    public mes: string,
+    public ano: string,
+    public nome_evento: string,
+    public descricao: string,
+    public publico_alvo: string,
+    public local: string,
+    public periodo: string,
+    public custo_previo: string,
+    public lead_previsto: string,
+    public sexec_id: number,
+    public tipo_evento_id: number,
+    public tipo_local_id: number,
+    public participacao_id: number,
+    public recursos_id: number
+  ){}
 }
