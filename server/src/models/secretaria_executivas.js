3010afe5e715121481d822924ab05622db3f7249
danielAraujo1406 <daniel.araujo@sedet.ce.gov.br>
2024-10-10 09:58:42 -0300

    novos relacionamentos

--- server/src/models/secretaria_executivas.js
+++ server/src/models/secretaria_executivas.js
@@ -4,16 +4,14 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Secretaria_Executivas extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       Secretaria_Executivas.hasMany(models.Evento, {
         foreignKey: 'sexec_id', as: 'ass_sexec_evento'
       });
-      // define association here
+      Secretaria_Executivas.hasMany(models.User, {
+        foreignKey: 'sexec_id', as: 'ass_sexec_user'
+      });
     }
   }
   Secretaria_Executivas.init({
