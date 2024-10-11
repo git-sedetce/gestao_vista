2d54bd1faa7ec8149a1c9838d9eff67c782859f1
danielAraujo1406 <daniel.araujo@sedet.ce.gov.br>
2024-10-11 10:21:32 -0300

    novas rotas

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,14 +5,20 @@ import { CadastroEventoComponent } from './components/paginas/cadastro-evento/ca
 import { CadastroAcompanhamentoComponent } from './components/paginas/cadastro-acompanhamento/cadastro-acompanhamento.component';
 import { ConsultaEventoComponent } from './components/paginas/consulta-evento/consulta-evento.component';
 import { ConsultaAcompanhamentoComponent } from './components/paginas/consulta-acompanhamento/consulta-acompanhamento.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { CadastroComponent } from './components/user/cadastro/cadastro.component';
+import { ResetComponent } from './components/user/reset/reset.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'cadastroevento', component: CadastroEventoComponent},
-  { path: 'cadastroacompanhamento', component: CadastroAcompanhamentoComponent},
-  { path: 'consultaeventos', component: ConsultaEventoComponent},
-  { path: 'consultaacompanhamento', component: ConsultaAcompanhamentoComponent}
+  { path: 'cadastroevento', component: CadastroEventoComponent },
+  { path: 'cadastroacompanhamento', component: CadastroAcompanhamentoComponent },
+  { path: 'consultaeventos', component: ConsultaEventoComponent },
+  { path: 'consultaacompanhamento', component: ConsultaAcompanhamentoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'reset', component: ResetComponent }
 ];
 
 @NgModule({
